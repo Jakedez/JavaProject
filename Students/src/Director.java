003bfeb07bbdd7f517fe731a824011f2642ba4cd
Jakedez <jakedez7@gmail.com>
2023-03-07 12:24:13 -0700

    Updated project

--- Students/src/Director.java
+++ Students/src/Director.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 
 public class Director {
-    // private ArrayList<Student> students = new ArrayList<Student>();
     private static HashMap<String, Student> students = new HashMap<String, Student>();
     private static Student activeStudent = null;
     private static boolean running = true;
@@ -13,6 +12,8 @@ public class Director {
         {
             add(new String("Add Student"));
             add(new String("Lookup Student"));
+            add(new String("Get Record Count"));
+            add(new String("Log out Student"));
             add(new String("Remove Student"));
             add(new String("Edit Student"));
             add(new String("Exit"));
@@ -26,12 +27,13 @@ public class Director {
 
     private static Student searchStudent(){
         System.out.println("Please search for a student record: ");
-        String name = scanner.nextLine();
+        String name = readString();
         if (students.containsKey(name)){
+            print("Student Found!");
             return students.get(name);
         }
         else{
-            print(String.format("No Record found for student with name \"%s\".", name));
+            print(String.format("No Record found for student with name \"%s\".\n", name));
             return null;
         }
     }
@@ -46,46 +48,144 @@ public class Director {
 
     private static void printActive(){
         if (activeStudent == null){
-            print("No student Account Selected.\n\n");
+            print("No student Account Selected.\n");
         }
         else{
-            print(String.format("Student: %s\nAge: %d\nMajor: %s\n\n", activeStudent.getName(), activeStudent.getAge(), activeStudent.getMajor()));
+            print(String.format("Student: %s\nAge: %d\nMajor: %s\nCredits: %d\n\n", activeStudent.getName(), activeStudent.getAge(), activeStudent.getMajor(), activeStudent.getCredits()));
         }
         return;
     }
 
-    private static void selectOption(){
-        try{
-            int option = 0;
-            print("Please Select an Option: ");
-            option = scanner.nextInt();
-            switch (option){
-                case 1:
+    private static void addStudent(){
+        print("Please enter the student's information: \n\n");
+        Student localStudent = new Student();
+        students.put(localStudent.getName(), localStudent);
+        return;
+    }
 
-                    break;
-                case 2:
-                    searchStudent();
-                    break;
-                case 3:
+    private static void diselectStudent(){
+        if (activeStudent != null){
+            activeStudent = null;
+            print("Student logged out!\n");
+        }
+        else{
+            print("No student currently logged in.\n");
+        }
+    }
 
-                    break;
-                case 4:
+    private static void removeStudent(){
+        if (activeStudent != null){
+            students.remove(activeStudent.getName());
+            activeStudent = null;
+            print("Account removed!");
+        }
+        else{
+            print("No student currently logged in.\n");
+        }
+        return;
+    }
 
+    private static void editStudent(){
+        boolean confirmed = false;
+        int choice = 0;
+        if (activeStudent != null){
+            while (!confirmed){
+                print("\n\n");
+                printActive();
+                print("1. Change Age");
+                print("2. Change Major");
+                print("3. Add Credits");
+                print("4. Change Credit Count");
+                print("5. Return to Main menu");
+                choice = readInt("\nPlease select an option: ");
+                switch (choice){
+                    case 1:
+                    activeStudent.setAge(readInt("Please enter a new age: "));
                     break;
-                case 5:
-                    running = false;
+                    case 2:
+                    print("Please Type a new Major: ");
+                    activeStudent.setMajor(readString());
                     break;
-                default:
-                    print("Please Select a Valid Option\n\n");
+                    case 3:
+                    activeStudent.addCredits(readInt("Please enter additional credits to add: "));
                     break;
-                
-                
+                    case 4:
+                    activeStudent.setCredits(readInt("Please enter total credits: "));
+                    break;
+                    case 5:
+                    confirmed = true;
+                    break;
+                    default:
+                    print("Please Select a valid option.");
+                    break;
+
+                }
+    
             }
         }
-        catch (Exception e){
-            scanner.next();
-            print("Please Select a Valid Option\n\n");
+        else{
+            print("There is no student logged in to modify.\n\n");
         }
+        return;
+    }
+
+    private static void getCount(){
+        print(String.format("Current Record Count: %d\n", students.size()));
+        return;
+    }
+    
+    private static int readInt(String prompt){
+        int number = 0;
+        boolean confirmed = false;
+        while (!confirmed){
+            try{
+                System.out.println(prompt);
+                number = Integer.parseInt(scanner.nextLine());
+                confirmed = true;
+            }
+            catch(Exception e){
+                System.out.println("Please type a valid number\n\n");
+            }
+        }
+        return number;
+    }
+
+    private static String readString(){
+        String string = scanner.nextLine();
+        return string;
+    }
+
+    private static void selectOption(){
+        int option = 0;
+        option = readInt("Please Select an Option: ");
+        switch (option){
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                activeStudent = searchStudent();
+                break;
+            case 3:
+                getCount();
+                break;
+            case 4:
+                diselectStudent();
+                break;
+            case 5:
+                removeStudent();
+                break;
+            case 6:
+                editStudent();
+                break;
+            case 7:
+                running = false;
+                break;
+            default:
+                print("Please Select a Valid Option\n\n");
+                break;
+        }
+            
+        
         return;
         
     }
--- Students/src/Student.java
+++ Students/src/Student.java
@@ -1,12 +1,46 @@
+import java.util.Scanner;
+
 public class Student {
     private String _name;
     private String _major;
     private int _age;
+    private int _credits;
+    private Scanner scanner = new Scanner(System.in);
     
 
-    public Student(){
-
+    private int readInt(String prompt){
+        int number = 0;
+        boolean confirmed = false;
+        while (!confirmed){
+            try{
+                System.out.println(prompt);
+                number = Integer.parseInt(scanner.nextLine());
+                
+                confirmed = true;
+            }
+            catch(Exception e){
+                System.out.println("Please type a valid number\n\n");
+            }
+        }
+        return number;
     }
+
+    private String readString(){
+        String string = "";
+        string = scanner.nextLine();
+        return string;
+    }
+
+
+    public Student(){
+        System.out.println("Please enter the student's name: ");
+        _name = readString();
+        _age = readInt("please enter the student's age (Years): ");
+        System.out.println("Please enter the student's major: ");
+        _major = readString();
+        _credits = readInt("please enter the student's credit count: ");
+    }
+
     public int getAge() {
         return _age;
     }
@@ -25,5 +59,14 @@ public class Student {
     }
     public void setMajor(String major){
         _major = major;
+    }
+    public int getCredits(){
+        return _credits;
+    }
+    public void setCredits(int credits){
+        _credits = credits;
+    }
+    public void addCredits(int credits){
+        _credits += credits;
     }
 }
